b3c5f28a0e8bbb9bce50abb9220d1791a9a23c1c
sbarrow825 <sbarrow825@berkeley.edu>
2020-07-19 20:36:49 -0700

    moved package.json and server.js to the root of the git repo

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// require("./develop/routes/apiRoutes")(app);
-require("./develop/routes/htmlRoutes")(app);
+// require("./Develop/routes/apiRoutes")(app);
+require("./Develop/routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
